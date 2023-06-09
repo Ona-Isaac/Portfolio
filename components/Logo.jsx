8d9b4a786f6d4638b4f94f4fedbecda4b4e9b8c8
Adrian <isaacona2@gmail.com>
2023-06-09 18:24:49 +0100

    update3

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -6,7 +6,7 @@ const MotionLink = motion(Link);
 const Logo = () => (
   <div className="flex items-center justify-center mt-2">
     <MotionLink
-      href="/"
+      href=""
       className="w-14 h-14 bg-white text-black flex items-center justify-center rounded-full text-2xl font-bold"
       whileHover={{
         scale: 1.05,
